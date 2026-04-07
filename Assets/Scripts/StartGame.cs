fffcafe8b7bf622b0d04d1107dc19d63ac1637cf
mayah992 <mah992@nau.edu>
2026-04-07 14:42:21 -0700

    Added apples

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class start_game : MonoBehaviour
+{
+    public List<GameObject> apples;
+
+    p
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnApples();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnApples()
+    {
+
+        foreach (GameObject prefab in apples)
+        {
+            float randomX = Random.Range(-50f, 50f);
+            float randomZ = Random.Range(70f, -60f);
+            Vector3 randomPos = new Vector3(randomX, -0.38f, randomZ);
+            Instantiate(prefab, randomPos, Quaternion.identity);
+        }
+    }
+}
